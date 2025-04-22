a35fcb7739e86fa6d31b44e9443b802eb2c27f39
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-21 21:53:07 -0300

    feature: load history appointments

--- src/modules/page/load.js
+++ src/modules/page/load.js
@@ -14,6 +14,50 @@ function renderClientInformation(card) {
     // clientPhoto.setAttribute("alt", `Foto de ${name}`);
 }
 
+function renderClientAppointmentHistory(appointmentHistory) {
+    const appointmentHistorySection = document.getElementById("cuts-history");
+
+    const appointmentCount = document.getElementById("history-count");
+    appointmentCount.textContent = `${appointmentHistory.length} cortes`;
+
+    const historyItemRemove = document.querySelectorAll("div.history-item");
+    historyItemRemove.forEach(historyItem => {
+        if (historyItem.parentNode) {
+            historyItem.parentNode.removeChild(historyItem);
+          }
+    })
+
+    appointmentHistory.forEach(appointment => {
+        const historyItem = document.createElement("div");
+        historyItem.classList.add("history-item");
+
+        const itemData = document.createElement("div");
+        itemData.classList.add("item-data");
+
+        const date = document.createElement("strong");
+        date.textContent = appointment.date;
+
+        const time = document.createElement("span");
+        time.textContent = appointment.time;
+
+        const checkIcon = document.createElement("div");
+        checkIcon.classList.add("checked-icon");
+        checkIcon.classList.add("gap-12");
+
+        const checkSvg = document.createElement("img");
+        checkSvg.setAttribute("src", './src/assets/icons/check-green.svg');
+
+        checkIcon.append(checkSvg);
+
+        itemData.append(date, time);
+        historyItem.append(itemData, checkIcon);
+
+        appointmentHistorySection.appendChild(historyItem);
+
+    });
+}
+
 export function load(card) {
     renderClientInformation(card);
+    renderClientAppointmentHistory(card.appointmentHistory)
 }
